69c31bc048dc33aafa89aee0418f0be32d8ad7df
Sane Aalam <saneraza78692@gmail.com>
2025-06-03 23:04:22 +0530

    added: write routers and controls in backend & API_error, asyncHandler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,10 @@
+
+// using async-await way
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export { asyncHandler }
