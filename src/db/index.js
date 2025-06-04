d5e570d6b0d96012c324eb18f2f7986db9d3b640
Sane Aalam <saneraza78692@gmail.com>
2025-06-04 12:10:47 +0530

    added : all setup configured

--- src/db/index.js
+++ src/db/index.js
@@ -2,9 +2,11 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js"
 
+const DB_URL = process.env.MONGODB_URI;
+
 const connectDB = async () =>{
     try{
-        const connectionInstanceDB = await mongoose.connect("mongodb+srv://saneraza78692:cjK5R9GUGef0cSFu@cluster0.mshzbft.mongodb.net/");
+        const connectionInstanceDB = await mongoose.connect(`${DB_URL}/${DB_NAME}`);
          console.log(`\n MongoDB connected !! DB HOST: ${connectionInstanceDB.connection.host}`);
     }catch(error){
         console.log("MONGODB connection FAILED ", error);
