a95871f69b97e79391a9907bea35cc4d38170b5d
Sane Aalam <saneraza78692@gmail.com>
2025-06-06 23:04:24 +0530

    update: register massage, learn multer file upload

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,8 @@ import {asyncHandler} from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async(req,res) =>{
     res.status(200).json({
-       massage:"okay"
+       massage:"Everything is working fine! OKAY",
+       value : 1,
     })
 })
 
